f2f9fc3cbb217e389f984f2fbc56ff17eb75d5b5
[hieutqph21650] <[hieutqph21650@fpt.edu.vn]>
2023-04-10 20:34:01 +0700

    deitRouters categorys

--- src/routes/category.js
+++ src/routes/category.js
@@ -3,11 +3,10 @@ import { getAll, get, create, remove, update } from "../controllers/category";
 import { checkPermission } from "../middlewares/checkPermission";
 const router = express.Router();
 
-router.route("/categorys").get(getAll).post(checkPermission, create);
-router
-  .route("/categorys/:id")
-  .get(get)
-  .delete(checkPermission, remove)
-  .patch(checkPermission, update);
+router.get("/categorys", getAll);
+router.get("/categorys/:id", get);
+router.post("/categorys/add", checkPermission, create);
+router.delete("/categorys/:id", checkPermission, remove);
+router.patch("/categorys/:id/update", checkPermission, update);
 
 export default router;
